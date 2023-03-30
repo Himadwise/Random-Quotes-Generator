1f96c6852a43cd18bb3f8f28325c399fa8bd6e89
himadwise <himadwise@gmail.com>
2023-03-30 16:42:12 +0300

    Random Quotes Generator is addded

---
+++ main.js
@@ -0,0 +1,417 @@
+const quotes = [{
+    quote: "2. Adin waa la dhibaa, ilna waa la dhawraa.",
+    author: "Maahmaah"
+},
+{
+    quote: "3. Abaal raaga rag baa leh, mid baaqdana haween baa leh, mid soo dhakhsadana xoolaa leh.",
+    author: "Maahmaah"
+},
+{
+    quote: "4. Abeesana dhul u eki bay wax ku dishaa, waadaadna dad u eki.",
+    author: "Maahmaah"
+},
+{
+    quote: "5. Dani waa seeto",
+    author: "Maahmaah"
+},
+{
+    quote: "6. sagaal nino walaala ah sideenaan",
+    author: "Gabaygii sayid Mohamed Abduralle hassan"
+},
+{
+    quote: "10.Xaashaa lilee beenta waa, laga xishoodaaye",
+    author: "Gabay"
+}
+    ,
+{
+    quote: "11.Hadalna wayga xaraansanayd, xigashadiisuye",
+    author: "Gabay"
+}
+    ,
+{
+    quote: "12.Kolba gabaygu meeshuu xuliyo, maanso xubinteeda",
+    author: "Gabay"
+}
+    ,
+{
+    quote: "13.Markaan helo xusuus muuqateen, xaraf ku miisaamo",
+    author: "Gabay"
+}
+    ,
+{
+    quote: "14.Xariiqdiisii waa inaan marshoo, xididkii raacaaye",
+    author: "Gabay"
+}
+    ,
+{
+    quote: "15.Maantana xaggii uu jiray baan, kaaga xaadirine",
+    author: "Gabay"
+}
+    ,
+{
+    quote: "16.Anoon xerada Soomaaliyeed, xumo ku sheegaynin",
+    author: "Gabay"
+}
+    ,
+{
+    quote: "17.Xog ogaaal dabciga waa inaan, ku xabbad qaadaaye",
+    author: "Gabay"
+}
+    ,
+{
+    quote: "18.Allow xoolo keen buu yidhaa, kii xaniif ahuye",
+    author: "Gabay"
+}
+    ,
+{
+    quote: "19.Xoogaa dharaartuu helee, loogu shubo xeedho",
+    author: "Gabay"
+}
+    ,
+{
+    quote: "20.Xusbadda u horraysaaba waa, xaali la ciyaare",
+    author: "Gabay"
+}
+    ,
+{
+    quote: "21.Xikmadaha asaageen watee, xoogga lagu yeesho",
+    author: "Gabay"
+}
+    ,
+{
+    quote: "22.Rag Allow maxaa naga xayira, waa xujiyo yaabe!",
+    author: "Gabay"
+}
+    ,
+{
+    quote: "23.wax badso wax beel leedahay",
+    author: "Maahmaah"
+}
+    ,
+{
+    quote: "24.inta aadn falin ka fiirso",
+    author: "Maahmaah"
+}
+    ,
+{
+    quote: "25.nin ku digay kuma dilin",
+    author: "Maahmaah"
+}
+    ,
+{
+    quote: "26.dantiis ma yaqaan maro duuga horteed dhamadaa",
+    author: "Maahmaah"
+}
+    ,
+{
+    quote: "27.naagu waa caruur caga weyn",
+    author: "Maahmaah"
+}
+    ,
+{
+    quote: "28.miro geed saran kuwo gunti kugu jiro looma dhaadsho",
+    author: "Maahmaah"
+}
+    ,
+{
+    quote: "29.canjeero seday u kala koraysaa loo qataa",
+    author: "Maahmaah"
+}
+    ,
+{
+    quote: "30.nin is faanshay waa ri is nuugtay",
+    author: "Maahmaah"
+},
+{
+    quote: '31. nimaan shaqaysan shaah waa ka xaraan',
+    author: 'Maahmaah'
+},
+
+{
+    quote: '32.  nin tuur le siduu u seexdo asaa yaqano',
+    author: 'Maahmaah'
+},
+{
+    quote: '33. beentada hore runtaada danbee u baas baxdaa',
+    author: 'Maahmaah'
+},
+{
+    quote: '34 haber fa dhido lagdan la fududaa',
+    author: 'Maahmaah'
+},
+{
+    quote: '35. kaadi badne waa loo gogol badiyaa',
+    author: 'Maahmaah'
+},
+{
+    quote: '36. rag hunguri buu isku raacaa',
+    author: 'Maahmaah'
+},
+{
+    quote: '37. rag waa qool dumarna waa meher',
+    author: 'Maahmaah'
+},
+{
+    quote: '38. rag waa ragii hore hdalna waa kii uu yiri',
+    author: 'Maahmaah'
+},
+{
+    quote: '39. rag walaalow wuxuu ku dhaamo lawaa',
+    author: 'Maahmaah'
+},
+{
+    quote: '40. rag waxaa ugu bfaan badan mar dhoof iyo duuul',
+    author: 'Maahmaah'
+},
+{
+    quote: '41. rag waxa ugu liita ciil kama baxe iyo calooshiid la ciyaare',
+    author: 'Maahmaah'
+},
+{
+    quote: '42. rag wuxu kugu bartoo kugu dilaa',
+    author: 'Maahmaah'
+},
+{
+    quote: '43 rag waa shaah dumar na waa sheeko',
+    author: 'Maahmaah'
+},
+{
+    quote: '44. rag garbaa kala saarta dumar na jaan',
+    author: 'Maahmaah'
+},
+{
+    quote: '45. rag waa arin keen iyo aqbalkeen',
+    author: 'Maahmaah'
+},
+{
+    quote: '46. naag waa balayo lo bahan yahay',
+    author: 'Maahmaah'
+},
+{
+    quote: '47. naagtaada oo kaa warantay waa maradaada oo kaa warantay',
+    author: 'Maahmaah'
+},
+{
+    quote: '48. naag waa caruur cag weyn',
+    author: 'Maahmaah'
+},
+{
+    quote: '49. nin aad taqaan yaa tahay lama dhaho',
+    author: 'Maahmaah'
+},
+{
+    quote: '50. uu mis Alle layaab malahan',
+    author: 'Maahmaah'
+},
+{
+    quote: '51. naag yaraan guur kuma reg , ragna yaraan ficil kuma lahan',
+    author: 'Maahmaah'
+},
+{
+    quote: '52. naag rag waa u wadda ciidan geelna waa u wada cano',
+    author: 'Maahmaah'
+},
+{
+    quote: '53 dumar iyo ciyaalba liiskuma cayaar baro',
+    author: 'Maahmaah'
+},
+{
+    quote: '54 Ma ciil sokeeyaa ku haya, mise ciddi hoosteedaa lagaa hayaa.',
+    author: 'Maahmaah'
+},
+{
+    quote: '55 ma maahmaahe waa loo maahmaahaa.',
+    author: 'Maahmaah'
+},
+{
+    quote: '56 Ma nimaan wax aqoon baad wax la qaybsataa, mise baddaasaad dhurtaa ',
+    author: 'Maahmaah'
+},
+{
+    quote: '57 maal yar iimaankaa lagu qabaa.',
+    author: 'Maahmaah'
+},
+{
+    quote: '58 Maalinkii intay ishaada gaadhaa la hadlaa, habeenkiina inta gacantaada gaadho',
+    author: 'Maahmaah'
+},
+{
+    quote: '59 Marna taagtaa marna tamartaa',
+    author: 'Maahmaah'
+},
+{
+    quote: '60 Madax meel ka sarraysa oo la taabtaa ma jirto',
+    author: 'Maahmaah'
+},
+{
+    quote: '61 Majo dhooqo leh, kuwo dhaqanaa ka roon',
+    author: 'Maahmaah'
+},
+{
+    quote: '62 Marba sidaad u jabtaa loo dhutiyaa',
+    author: 'Maahmaah'
+},
+{
+    quote: '63 Martidu marka hore reerkaa ka naxa marka dambena iyagaa kala naxda',
+    author: 'Maahmaah'
+},
+{
+    quote: '64 Maxay ka nuugtaa ama ku nuugtaa, bushmo iyo naaso midna maleh.',
+    author: 'Maahmaah'
+},
+{
+    quote: '65 meel hoo doonaysa hadal wax kama taro',
+    author: 'Maahmaah'
+},
+{
+    quote: '66 Meelaad madaxeeda tahay, majo lama noqdo.',
+    author: 'Maahmaah'
+},
+{
+    quote: '67 meeshii cir laga filaayey ceeryaamo ka timi.',
+    author: 'Maahmaah'
+},
+{
+    quote: '68 Midho geed saaran, kuwo guntiga ku jira looma qubo.',
+    author: 'Maahmaah'
+},
+{
+    quote: '69 Markhaatina marag baa la dhigtaa masaajidna dhagax.',
+    author: 'Maahmaah'
+},
+{
+    quote: '70 mukulaal minankeeda joogtaa miciyo libaax bay leedahay',
+    author: 'Maahmaah'
+},
+{
+    quote: '71 mukulaashu dooliga danteeday u dabataa',
+    author: 'Maahmaah'
+},
+{
+    quote: '72 Majo dhooqo midba marbaa lasiibaa',
+    author: 'Maahmaah'
+},
+{
+    quote: '73 Doqoni hooskeed bay har mooddaa',
+    author: 'Maahmaah'
+},
+{
+    quote: '74 Doqoni kula tol maaha',
+    author: 'Maahmaah'
+},
+{
+    quote: '75 Doqoni halkay tegeyso ayay ku geysaa',
+    author: 'Maahmaah'
+},
+{
+    quote: '76 Doqoni meeshii lagu dagaalay hadday ogayd,meeshii lagu heshiiyey ma ogayn.',
+    author: 'Maahmaah'
+},
+{
+    quote: '77 Doqoni sokeeye maaha, hilibna sooryo maaha socodna raaxo maaha',
+    author: 'Maahmaah'
+},
+{
+    quote: '78 Doqoni wixii u daran bay illowdaa',
+    author: 'Maahmaah'
+},
+{
+    quote: '79 Doqoni xargaha lagu xirayo xusulladay ku sidata',
+    author: 'Maahmaah'
+},
+{
+    quote: '80 Doqonnimo daawo maleh',
+    author: 'Maahmaah'
+},
+{
+    quote: '81 Abuur iyo waano ,abuur baa horreysey.',
+    author: 'Maahmaah'
+},
+{
+    quote: '82 Aan wada hadalno, waa aan heshiino.',
+    author: 'Maahmaah'
+},
+{
+    quote: '83 Abaal raaga rag baa leh.',
+    author: 'Maahmaah'
+},
+{
+    quote: '84 Af Daboolan, Dahab waaye.',
+    author: 'Maahmaah'
+},
+{
+    quote: '85 Af jooga looma adeego.',
+    author: 'Maahmaah'
+},
+{
+    quote: '86 Af macaan gacan macaan baa dhaanta.',
+    author: 'Maahmaah'
+},
+{
+    quote: '87 Af wax cunay xishoo.',
+    author: 'Maahmaah'
+},
+{
+    quote: '88 Alif kaa xumaaday Albaqra kaagama hadho.',
+    author: 'Maahmaah'
+},
+{
+    quote: '89 Allaah aamine ma iisho.',
+    author: 'Maahmaah'
+},
+{
+    quote: '90 Allow nin aan wax ogeyn ha cadaabin.',
+    author: 'Maahmaah'
+},
+{
+    quote: '91 Ama afeef hore lahow ama adkaysi danbe.',
+    author: 'Maahmaah'
+},
+{
+    quote: '92Aqoon laaani waa iftiin laaan.',
+    author: 'Maahmaah'
+},
+{
+    quote: '93 Aqoon xumo abaar ka daran.',
+    author: 'Maahmaah'
+},
+{
+    quote: '94 Arin nin la toosan, nin la tuur  leh.',
+    author: 'Maahmaah'
+},
+{
+    quote: '95 Aroos lagama raago, lagumana raago.',
+    aurthor: 'Maahmaah'
+},
+{
+    quote: '96 Ayax teg eelna reeb.',
+    author: 'Maahmaah'
+},
+{
+    quote: '97 Adduun waa hadhka labadiisa gelin.',
+    author: 'Maahmaah'
+},
+{
+    quote: '98 Aqooni waa awood.',
+    author: 'Maahmaah'
+},
+{
+    quote: '99 Ammin dumay dib uma soo laabto.',
+    author: 'Maahmaah'
+},
+{
+    quote: '100 Aammusnaantu waxay astaan u tahay oggolaansho .',
+    author: 'Maahmaah'
+},
+{
+    Quote: '101 dadaalaa wuu gaaaraa',
+    author: 'Sharm Poi'
+}
+]
+
+generate = document.querySelector('.generator')
+generate.addEventListener('click', () => {
+    let random = Math.floor(Math.random() * quotes.length);
+    document.querySelector('.quote').innerHTML = quotes[random].quote
+    document.querySelector('.author').innerHTML = quotes[random].author
+})
